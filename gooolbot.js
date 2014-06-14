d9a21851a62900e17cc4ad0fa0b898862625b0e5
jeff oconnell <jeff.oconnell@gmail.com>
2014-06-14 19:19:55 -0400

    gooooooool

--- gooolbot.js
+++ gooolbot.js
@@ -15,17 +15,48 @@ var config = require( 'config' )
 
 config._gb = { follow : "89225092", screen_name : "UnivisionSports" }
 
-var hipchatter = new Hipchatter( config.hipchat.auth_token )
-var T          = new Twit( config.twitter )
+//
+// hipchat
+//
 
-var stream = T.stream( 'statuses/sample', { follow : config._gb.follow } )
+var hipchatter = new Hipchatter( config.hipchat.auth_token )
+
+hipchatter.rooms(
+	function( err, remote_rooms )
+	{
+		// fatal error
+        if ( err ) { console.log( "[hipchat] err : " + err ) ; process.exit( 1 ) }
+        
+        // report on configured rooms
+		for ( var room in config.hipchat.rooms )
+		{
+			var configured_name = config.hipchat.rooms[room].name ;
+
+			for ( var remote in remote_rooms )
+			{
+				if ( configured_name == remote_rooms[remote].name ) {
+					console.log( "[hipchat] found room: " + configured_name ) ;
+				}
+			}
+		}
+
+    }
+)
+
+//
+// twitter
+//
+
+var T = new Twit( config.twitter )
+
+var stream = T.stream( 'statuses/filter', { follow : config._gb.follow } )
 
 stream.on( 'tweet', function (tweet) {
 
 	if ( tweet.user.screen_name == config._gb.screen_name ) 
 	{
 		// log all @UnivisionSports tweets so we know gooolbot is alive
-		console.log( "[tweet] @" + tweet.user.screen_name + " : " + tweet.text )
+		console.log( "[twitter] @" + tweet.user.screen_name + " : " + tweet.text )
 
 		var regex = /(Go{4,}l)/
 		var result = tweet.text.match( regex )
@@ -57,8 +88,8 @@ stream.on( 'tweet', function (tweet) {
 					notify         : true
 				}, 
 				function ( err ) { 
-					if ( err ) { console.log( err ) ; return } 
-					console.log( "[" + r.name + "] " + message ) ;
+					if ( err ) { console.log( "[hipchat] " + r.name + " : " + err ) ; return } 
+					console.log( "[hipchat] " + r.name + " : " + message ) ;
 				}
 			)
 
