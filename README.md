55b3d3cac31f47a81f22c8dd8422f337df7e2b9e
jeff oconnell <jeff.oconnell@gmail.com>
2014-06-14 18:11:33 -0400

    gooooooool

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-gooolbot
-========
+
+//
+// gooolbot
+//
+// relay goooooools to hipchat room
+//
+// author: @jefforulez
+//
+
+== description ==
+
+the univision world cup streams ( http://futbol.univision.com/ ) 
+seem to be on a one minute delay.  the epsn broadcasts are another sixty seconds
+behind that. 
+
+[@univisionsports](https://twitter.com/UnivisionSports), on the other hand, 
+seems to be tweeting in faster-than-real-time.
+
+this bot will notify you via [@hipchat](https://twitter.com/hipchat) that you should 
+immediately command-tab to that browser window that you weren't doing a good job 
+hiding from your boss anyway.
+
+== requirements ==
+
+- a twitter app and associated auth tokens - https://apps.twitter.com/
+- a hipchat account with a personal api token - https://www.hipchat.com/account/api
+- a hipchat room that you own with an auth token - https://www.hipchat.com/rooms
+
+== setup ==
+
+* git clone https://github.com/jefforulez/gooolbot
+* cd ./gooolbot
+* npm install hipchatter twit config
+* cp config/sample.default.json config/default.json
+* vim config/default.json
+* node ./gooolbot.js
+
+
+
+
---
+++ gooolbot.js
@@ -0,0 +1,59 @@
+
+//
+// gooolbot.js - https://github.com/jefforulez/gooolbot
+//
+// written by @jefforulez
+//
+//
+
+'use strict' ;
+
+var Hipchatter = require( 'hipchatter' )
+var Twit = require('twit')
+
+var config = require( 'config' )
+
+config._gb = { follow : "89225092", screen_name : "UnivisionSports" }
+
+var hipchatter = new Hipchatter( config.hipchat.auth_token )
+var T          = new Twit( config.twitter )
+
+var stream = T.stream( 'statuses/filter', { follow : config._gb.follow })
+
+stream.on( 'tweet', function (tweet) {
+
+	if ( tweet.user.screen_name == config._gb.screen_name ) 
+	{
+		console.log( "[debug]" + tweet.text )
+
+		var regex = /(Go{4,}l)/
+		var result = tweet.text.match( regex )
+				
+		if ( result == null ) {
+			return
+		}
+		
+		// debug
+		console.log( tweet )
+
+		var url = "https://twitter.com/" + screen_name + "/status/" + tweet.id_str
+		
+		var message = result[0] + "!!!! " + tweet.text + "<a href=" + url + ">" + url + "</a>"
+
+		for ( var room in config.hipchat.rooms )
+		{
+			hipchatter.notify(
+				room.name,
+				{
+					token          : room.token,
+					message        : message,
+					message_format : 'html',
+					notify         : true
+				}, 
+				function ( err ) { if ( err ) console.log( err ) }
+			)
+		}
+
+	}
+	
+})
